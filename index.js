b7daf8e22c43f81cbbd31476070d67f01cfd9481
Allan Araújo <allan.hal@gmail.com>
2017-11-16 02:25:05 -0200

    Buildando com Heroku

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const compression = require('compression')
+
+const app = express()
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(compression())
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'))
+})
+
+app.listen(process.env.PORT || '4200');
